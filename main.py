9484c633711eb8def1c3c32f38adf3f88013e984
Nchia-Emmanuela <83321016+Nchia-Emmanuela@users.noreply.github.com>
2021-09-28 13:41:38 +0100

    joining all words

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 def spin_words(sentence):
    split_word = sentence.split('')
-   new_word = []
+   new_words = []
    for word in split_word:
        if len(word) >= 2:
-           new_word.append(word[::-1])
+           new_words.append(word[::-1])
+        else:
+            new_words.append(word)
+    return ' '.join(new_word)
